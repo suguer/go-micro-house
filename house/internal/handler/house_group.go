8231475cc35efbe2ec73186bf89aee48076df90b
home <home@szj.com>
2023-01-15 16:29:48 +0800

    test: 测试service接口使用

--- house/internal/handler/house_group.go
+++ house/internal/handler/house_group.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"fmt"
+	"house/internal/model"
 	"house/internal/service"
 )
 
@@ -13,8 +15,47 @@ func NewHouseGroupService() *HouseGroupService {
 	return &HouseGroupService{}
 }
 
-func (*HouseGroupService) Index(ctx context.Context, req *service.HouseGroupRequest) (resp *service.HouseGroupResponse, err error) {
+func (*HouseGroupService) Index(ctx context.Context, req *service.HouseGroupIndexRequest) (resp *service.HouseGroupIndexResponse, err error) {
+	resp = new(service.HouseGroupIndexResponse)
+	var list []model.HouseGroup
+	var pagination service.Pagination
+	err = model.DB.Scopes(model.Paginate(int(req.PageSize), int(req.Current))).
+		Where("user_id=?", req.UserId).
+		Find(list).Limit(-1).Offset(-1).
+		Count(&pagination.Total).Error
+	return resp, err
+}
+func (*HouseGroupService) Create(ctx context.Context, req *service.HouseGroupRequest) (resp *service.HouseGroupResponse, err error) {
+	fmt.Printf("req.Name: %v\n", req.Name)
 	resp = new(service.HouseGroupResponse)
-	resp.Code = 440
-	return resp, nil
+	var group model.HouseGroup
+	model.DB.FirstOrCreate(&group, model.HouseGroup{
+		Name:   req.Name,
+		UserId: uint(req.UserId),
+	})
+	resp.HouseGroupModel = model.BuildHouseGroup(&group)
+	return resp, err
+}
+
+func (*HouseGroupService) Remove(ctx context.Context, req *service.HouseGroupRequest) (resp *service.HouseGroupResponse, err error) {
+	resp = new(service.HouseGroupResponse)
+	var group model.HouseGroup
+	model.DB.Create(&group)
+	resp.HouseGroupModel = model.BuildHouseGroup(&group)
+	return resp, err
+}
+
+func (*HouseGroupService) Join(ctx context.Context, req *service.HouseGroupRequest) (resp *service.HouseGroupResponse, err error) {
+	resp = new(service.HouseGroupResponse)
+	var group model.HouseGroup
+	group.ID = uint(req.GroupId)
+	err = group.Join(uint(req.HouseId))
+	return resp, err
+}
+func (*HouseGroupService) Leave(ctx context.Context, req *service.HouseGroupRequest) (resp *service.HouseGroupResponse, err error) {
+	resp = new(service.HouseGroupResponse)
+	var group model.HouseGroup
+	group.ID = uint(req.GroupId)
+	err = group.Leave(uint(req.HouseId))
+	return resp, err
 }
--- house/internal/handler/house_test.go
+++ house/internal/handler/house_test.go
@@ -1,1 +1,80 @@
 package handler
+
+import (
+	"context"
+	"fmt"
+	"house/config"
+	"house/internal/model"
+	"house/internal/service"
+	"testing"
+	"time"
+)
+
+func TestCreateHouse(t *testing.T) {
+	config.InitConfig()
+	model.InitDB()
+	var ctx context.Context
+	ser := NewHouseService()
+	resp, err := ser.Create(ctx, &service.HouseRequest{
+		Data: &service.HouseModel{
+			UserId: 1,
+			Title:  "20230115",
+		},
+	})
+	if resp.Code != 0 {
+		t.Fatal(resp)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("resp: %v\n", resp)
+}
+
+func TestUpdateHouse(t *testing.T) {
+	config.InitConfig()
+	model.InitDB()
+	var ctx context.Context
+	ser := NewHouseService()
+	resp, err := ser.Update(ctx, &service.HouseRequest{
+		Data: &service.HouseModel{
+			Id:     1,
+			UserId: 1,
+			Title:  time.Now().Format("2006-01-02 15:04:05"),
+		},
+	})
+	if resp.Code != 0 {
+		t.Fatal(resp.Code)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("resp: %v\n", resp)
+}
+
+func TestHouseGroup(t *testing.T) {
+	config.InitConfig()
+	model.InitDB()
+	var ctx context.Context
+	ser := NewHouseGroupService()
+	resp, err := ser.Create(ctx, &service.HouseGroupRequest{
+		Name:   time.Now().Format("2006-01-02 15:04:05"),
+		UserId: 1,
+	})
+	if err != nil {
+		t.Log(err)
+	}
+	ser.Join(ctx, &service.HouseGroupRequest{
+		GroupId: resp.HouseGroupModel.Id,
+		HouseId: 1,
+	})
+	if err != nil {
+		t.Log(err)
+	}
+	ser.Leave(ctx, &service.HouseGroupRequest{
+		GroupId: resp.HouseGroupModel.Id,
+		HouseId: 1,
+	})
+	if err != nil {
+		t.Log(err)
+	}
+}
