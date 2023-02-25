1b2d3c16d670465e560145844d404384d1161353
dsuzejian <szj@ericma.onaliyun.com>
2023-02-25 13:57:57 +0800

    fix:  "&loc=Local"

--- crontab/internal/model/db_init.go
+++ crontab/internal/model/db_init.go
@@ -22,7 +22,7 @@ func InitDB() {
 	username := viper.GetString("mysql.username")
 	password := viper.GetString("mysql.password")
 	charset := viper.GetString("mysql.charset")
-	dsn := strings.Join([]string{username, ":", password, "@tcp(", host, ":", port, ")/", database, "?charset=" + charset + "&parseTime=true"}, "")
+	dsn := strings.Join([]string{username, ":", password, "@tcp(", host, ":", port, ")/", database, "?charset=" + charset + "&parseTime=true", "&loc=Local"}, "")
 	err := Database(dsn)
 	if err != nil {
 		fmt.Println(err)
